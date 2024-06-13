d9ea343e1423a89a9231a498dba29bcd1d4a906f
websitedevelopers24 <mohammad18osman@outlook.com>
2024-06-13 20:02:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function openPDF(country) {
     const pdfLinks = {
-        'Schengen': 'pdfs/SCHENGEN VISA.pdf',
-        'Egypt': 'pdfs/EGYPT VISA.pdf',
-        'China': 'pdfs/CHINA VISA.pdf',
-        'United_States': 'pdfs/UNITED STATES VISA.pdf',
-        'Australia': 'pdfs/AUSTRALIA VISA.pdf',
-        'Thailand': 'pdfs/THAILAND VISA.pdf',
-        'Sri_Lanka': 'pdfs/SRI LANKA VISA.pdf',
-        'Dubai': 'pdfs/DUBAI VISA.pdf',
-        'Armenia': 'pdfs/ARMENIA VISA.pdf',
-        'Bali': 'pdfs/BALI VISA.pdf',
-        'Brazil': 'pdfs/BRAZIL VISA.pdf',
-        'Canada': 'pdfs/CANADA VISA.pdf',
-        'Cyprus': 'pdfs/CYPRUS VISA.pdf',
-        'India': 'pdfs/INDIA VISA.pdf',
-        'Japan': 'pdfs/JAPAN VISA.pdf',
-        'Tunis': 'pdfs/TUNIS VISA.pdf',
-        'United_Kingdom': 'pdfs/UNITED KINGDOM VISA.pdf',
-        'Turkey': 'pdfs/TURKEY VISA.pdf'
+        'Schengen': 'Pdfs/SCHENGEN VISA.pdf',
+        'Egypt': 'Pdfs/EGYPT VISA.pdf',
+        'China': 'Pdfs/CHINA VISA.pdf',
+        'United_States': 'Pdfs/UNITED STATES VISA.pdf',
+        'Australia': 'Pdfs/AUSTRALIA VISA.pdf',
+        'Thailand': 'Pdfs/THAILAND VISA.pdf',
+        'Sri_Lanka': 'Pdfs/SRI LANKA VISA.pdf',
+        'Dubai': 'Pdfs/DUBAI VISA.pdf',
+        'Armenia': 'Pdfs/ARMENIA VISA.pdf',
+        'Bali': 'Pdfs/BALI VISA.pdf',
+        'Brazil': 'Pdfs/BRAZIL VISA.pdf',
+        'Canada': 'Pdfs/CANADA VISA.pdf',
+        'Cyprus': 'Pdfs/CYPRUS VISA.pdf',
+        'India': 'Pdfs/INDIA VISA.pdf',
+        'Japan': 'Pdfs/JAPAN VISA.pdf',
+        'Tunis': 'Pdfs/TUNIS VISA.pdf',
+        'United_Kingdom': 'Pdfs/UNITED KINGDOM VISA.pdf',
+        'Turkey': 'Pdfs/TURKEY VISA.pdf'
     };
     const link = pdfLinks[country];
     if (link) {
